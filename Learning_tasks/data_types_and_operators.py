98820a39c552439f5f41c9b84e81b25c7ad8353d
Nathan Hastings <nathanhastings412@gmail.com>
2025-07-15 16:56:58 +0100

    data types and operators task added

---
+++ Learning_tasks/data_types_and_operators.py
@@ -0,0 +1,20 @@
+# Understanding Operators
+# Operators indicate what action or operation to perform
+# Operands indicate what items to apply the action to
+
+x = 5
+y = 1
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+
+print(x <= y)
+print(x < y)
+print(x >= y)
+print(x > y)
+print(x == y)
+print(x != y)
+
